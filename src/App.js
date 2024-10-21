80b37328df4af780cfad64d36471557fe054af72
ryderthieu <ryderthieu@gmai.com>
2024-10-21 08:59:09 +0700

    update header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import Dashboard from './pages/Dashboard/Dashboard'
 const App = () => {
 
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path = '/' element = {<Homepage />}/>
       <Route path = '/login' element = {<Login />}/>
       <Route path = '/register' element = {<Register />}/>
+      <Route path = '/dashboard' element = {<Dashboard />} />
     </Routes>
     </>
   ) 
