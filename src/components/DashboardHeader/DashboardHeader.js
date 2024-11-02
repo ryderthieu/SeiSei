82ec7aa57f712f01dffe5705c9e210f392370eff
Puwuu22 <22521235@gm.uit.edu.vn>
2024-11-02 20:06:06 +0700

    update dashboard header

--- src/components/DashboardHeader/DashboardHeader.js
+++ src/components/DashboardHeader/DashboardHeader.js
@@ -1,8 +1,15 @@
+import './DashboardHeader.scss'
+
+
 const DashboardHeader = () => {
     return (
         <>
-            <h1>
-                Header
+            <h1 className="dashboard-header">
+                <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+                <ion-icon name="notifications-outline"></ion-icon>
+                <div className="dashboard-header__user">
+                    <div className="dashboard-header__user-avatar"></div>
+                </div>
             </h1>
         </>
     )
