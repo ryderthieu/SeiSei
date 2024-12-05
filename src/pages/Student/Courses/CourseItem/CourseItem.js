7b1f6bfdfab5e7d2d92fa9d48e2535ae7d262d08
itsnhyyy <22521038@gm.uit.edu.vn>
2024-12-05 09:30:04 +0700

    update

--- src/pages/Student/Courses/CourseItem/CourseItem.js
+++ src/pages/Student/Courses/CourseItem/CourseItem.js
@@ -1,9 +1,11 @@
 import style from './CourseItem.module.scss';
+import Rating from 'react-rating-stars-component';
 import { useState } from 'react';
-import { FullContentCard } from '../../../../components/Card/Card'; // Import FullContentCard thay cho ExtendableCard
+import { FullContentCard } from '../../../../components/Card/Card';
 import { useNavigate } from 'react-router-dom';
 import TopTabNavigation from '../../../../components/TopTabNavigation/TopTabNavigation';
 import img from '../../../../assets/images/art.png';
+import tutorAvatar from '../../../../assets/images/tutor.jpg';
 
 const TopTab = ['Lớp học', 'Thông tin gia sư', 'Hỗ trợ học viên'];
 
@@ -50,6 +52,16 @@ const CourseItem = () => {
     }
   };
 
+  const [rating, setRating] = useState(0);
+
+  <Rating
+    count={5}
+    value={rating}
+    onChange={(newRating) => setRating(newRating)}
+    size={30}
+    activeColor="#ffd700"
+  />;
+
   return (
     <div className={style.container}>
       <div className={style.header}>
@@ -60,7 +72,6 @@ const CourseItem = () => {
       <div className={style.content}>
         <TopTabNavigation data={TopTab} activeTab={tab} onTabChange={setTab} />
 
-        {/* Nội dung hiển thị theo từng tab */}
         {tab === 0 && (
           <div>
             <div className={style.requestCard}>
@@ -70,14 +81,17 @@ const CourseItem = () => {
             <div className={style.headerList}>DANH SÁCH BÀI KIỂM TRA</div>
             <div className={style.testList}>
               <div className={style.testSelect}>
+                <label className={style.title}>Chọn bài kiểm tra</label>
                 <select className={style.select}>
                   <option value="">Chọn bài kiểm tra</option>
                   <option value="test1">Bài kiểm tra 1</option>
                   <option value="test2">Bài kiểm tra 2</option>
                 </select>
               </div>
+              <label className={style.title}>Bài làm</label>  
               <div className={style.testFile}>
-                <label>Bài làm:</label>
+                <ion-icon name="cloud-upload-outline"></ion-icon>
+                <label>Thêm bài nộp</label>
                 <input type="file" onChange={handleFileChange} />
               </div>
               <div className={style.buttons}>
@@ -91,18 +105,32 @@ const CourseItem = () => {
         {tab === 1 && (
           <div>
             <div className={style.tutorInfo}>
-              <h2>Nguyễn Văn A</h2>
-              <p><strong>Giới tính:</strong> Nam</p>
-              <p><strong>Bằng cấp:</strong> Ielts 7.0</p>
-              <p><strong>Giới thiệu:</strong> Sinh viên năm 4 trường Đại học Khoa học Xã hội & Nhân văn, chuyên ngành Ngôn ngữ Anh.</p>
-            </div>
-            <div className={style.supportButtons}>
-                <label htmlFor="details">Đánh giá gia sư:</label>
-                <textarea id="details" name="details" rows="4"></textarea>
+              <img className={style.tutorAvatar} src={tutorAvatar} alt="Avatar của gia sư" />
+              <div className={style.tutorDetails}>
+                <h2>Nguyễn Văn A</h2>
+                <p><strong>Giới tính:</strong> Nam</p>
+                <p><strong>Bằng cấp:</strong> Ielts 7.0</p>
+                <p><strong>Giới thiệu:</strong> Sinh viên năm 4 trường Đại học Khoa học Xã hội & Nhân văn, chuyên ngành Ngôn ngữ Anh.</p>
               </div>
-              <button type="submit" className={style.submitButton}>
-                Gửi
-              </button>
+            </div>
+
+            <div className={style.rating}>
+              <label>ĐÁNH GIÁ GIA SƯ:</label>
+              <Rating
+                count={5}
+                onChange={(newRating) => console.log(`Đánh giá: ${newRating}`)}
+                size={30}
+                activeColor="#ffd700"
+              />
+            </div>
+
+            <div className={style.supportButtons}>
+              <label htmlFor="details">Ý KIẾN NHẬN XÉT:</label>
+              <textarea id="details" name="details" rows="4"></textarea>
+            </div>
+            <button type="submit" className={style.submitButton}>
+              Thêm nhận xét
+            </button>
           </div>
         )}
 
