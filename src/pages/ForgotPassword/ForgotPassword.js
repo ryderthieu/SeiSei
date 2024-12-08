0a2e04a80573ca4d0de7d3f11d3458484fb8bcd5
Puwuu22 <22521235@gm.uit.edu.vn>
2024-12-08 15:38:30 +0700

    update forgot passw

--- src/pages/ForgotPassword/ForgotPassword.js
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -1,11 +1,17 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./ForgotPassword.scss";
 import loginImg from "../../assets/images/cuate.png";
-import TextInput from "../../components/TextInput/TextInput";
 
 const ForgotPassword = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/verification-code");
+  };
+
   return (
     <div className="forgot-password">
       <div className="forgot-password__background">
@@ -20,22 +26,24 @@ const ForgotPassword = () => {
         <p className="forgot-password__subtitle">
           Vui lòng nhập Email bạn đã đăng ký
         </p>
-        <form>
+        <form onSubmit={handleSubmit} className="forgot-password__form">
           <div className="forgot-password__wrapper">
-            <label className="forgot-password__label" forHTML="email">
+            <label className="forgot-password__label" htmlFor="email">
               Email
             </label>
             <input
               id="email"
               className="forgot-password__input"
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
-        </form>
-        <Link to="/verification-code"  className="forgot-password__submit">
+          <button type="submit" className="forgot-password__submit">
             Tiếp tục
-        </Link>
+          </button>
+        </form>
       </div>
     </div>
   );
