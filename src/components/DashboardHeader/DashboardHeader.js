6784aad8aeb35cd69cc6558a478b9c57db1ad3e4
Puwuu22 <22521235@gm.uit.edu.vn>
2024-12-09 14:35:03 +0700

    update

--- src/components/DashboardHeader/DashboardHeader.js
+++ src/components/DashboardHeader/DashboardHeader.js
@@ -21,19 +21,18 @@ const DashboardHeader = () => {
         setShowMessages(false);
     }
 
-
     return (
         <>
             <div className="dashboard-header">
-                <div className="icon-wrapper" onClick={toggleMessages}>
-                    <ion-icon name="chatbubble-ellipses"></ion-icon>
-                    <span className="icon-badge">5</span>
-                    <div className="icon-label">Tin nhắn</div>
+                <div className="dashboard-header__icon-container" onClick={toggleMessages}>
+                    <ion-icon name="chatbubble-ellipses" className="dashboard-header__icon"></ion-icon>
+                    <span className="dashboard-header__badge">5</span>
+                    <div className="dashboard-header__label">Tin nhắn</div>
                 </div>
-                <div className="icon-wrapper" onClick={toggleNotifications}>
-                    <ion-icon name="notifications"></ion-icon>
-                    <span className="icon-badge">4</span>
-                    <div className="icon-label">Thông báo</div>
+                <div className="dashboard-header__icon-container" onClick={toggleNotifications}>
+                    <ion-icon name="notifications" className="dashboard-header__icon"></ion-icon>
+                    <span className="dashboard-header__badge">4</span>
+                    <div className="dashboard-header__label">Thông báo</div>
                 </div>
                 <div className="dashboard-header__user">
                     <div className="dashboard-header__user-avatar"></div>
